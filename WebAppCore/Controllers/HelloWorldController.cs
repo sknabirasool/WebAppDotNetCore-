d3a0a6fbcf57efc7b6969c131d3284d215271197
sknabirasool <sknabirasool104@gmail.com>
2022-05-23 18:51:35 +0530

    first commit

---
+++ WebAppCore/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index(String name="Nabi Rasool",int numTimes=8)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+       
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ WebAppCore/Data/WebAppCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppCore.Models;
+
+namespace WebAppCore.Data
+{
+    public class WebAppCoreContext : DbContext
+    {
+        public WebAppCoreContext (DbContextOptions<WebAppCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppCore.Models.Student>? Student { get; set; }
+    }
+}
